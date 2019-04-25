f6c618bff2cd520e22a957a4f980140a33c5eba1
EfrainMario <efrainmario345@gmail.com>
2019-04-25 16:05:08 +0100

    Access-Control-Allow-Origin

--- src/controller/LojaController.php
+++ src/controller/LojaController.php
@@ -24,7 +24,7 @@ class LojaController{
 
         if ( $this->hasFiltersLogin($queryParams) ) {
             $resultado = $this->lojaDao->getLojaQueryLogin($queryParams);
-            $this->response->withHeader("Access-Control-Allow-Origin", $this->request->getHeader("Origin"));
+            $this->response->withHeader("Access-Control-Allow-Origin", '*');//$this->request->getHeader("Origin"));
             $this->response->withHeader("Access-Control-Allow-Credentials", "true");
             $this->response->withHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
             $this->response->withHeader("Access-Control-Max-Age", "3600");
