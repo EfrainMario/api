8d8860003a45fcb5453ed3aa8ea79a0b5d79c79f
EfrainMario <46673851+EfrainMario@users.noreply.github.com>
2019-04-27 23:43:16 +0100

    Test 1

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $app = new \Slim\App();
 date_default_timezone_set( "Africa/Luanda");
 
 header("Access-Control-Allow-Origin: https://lubeasy.herokuapp.com");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
