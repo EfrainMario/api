36146c0beca5bfb0937247f956631d132fc57263
VectisStartup <47024109+VectisStartup@users.noreply.github.com>
2019-04-27 11:48:07 +0100

    Acess Control

--- src/helpers/GeralHelper.php
+++ src/helpers/GeralHelper.php
@@ -54,7 +54,7 @@ function resultObject($resultado, $response){
     if ( $resultado != null ) {
         $clienteObject = $resultado[0];
 
-        return $response->withHeader('Access-Control-Allow-Origin', 'https://lubeasy.herokuapp.com')
+        return $response->withHeader('Access-Control-Allow-Origin', '*')
                 ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
                 ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')->withJson($clienteObject, 200);
 
