b64863be70787511af1f71892c90e01a880e6de9
EfrainMario <46673851+EfrainMario@users.noreply.github.com>
2019-04-27 23:21:24 +0100

    Delete acess control

--- src/helpers/GeralHelper.php
+++ src/helpers/GeralHelper.php
@@ -54,9 +54,7 @@ function resultObject($resultado, $response){
     if ( $resultado != null ) {
         $clienteObject = $resultado[0];
 
-        return $response->withHeader('Access-Control-Allow-Origin', '*')
-                ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')->withJson($clienteObject, 200);
+        return $response->withJson($clienteObject, 200);
 
     } else {
         return $response->withStatus(404);
