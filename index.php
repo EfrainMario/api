cb927f64a5eb26b0fc9041b34e3eba3b48bcf8f7
EfrainMario <46673851+EfrainMario@users.noreply.github.com>
2019-04-27 23:31:18 +0100

    Changed Sucessful
    Adicionando o Access-Control-Allow-Origin: https://lubeasy.herokuapp.com na linha 18, para a cors policy.

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ require __DIR__ . '/vendor/autoload.php';
 $app = new \Slim\App();
 date_default_timezone_set( "Africa/Luanda");
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://lubeasy.herokuapp.com");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
